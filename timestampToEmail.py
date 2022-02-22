0aa7f872ceef04660c66164427389313a9f5fdb3
Zakariya Khan <zakariyavkhan@gmail.com>
2022-02-22 09:00:58 -0800

    Formatting

--- timestampToEmail.py
+++ timestampToEmail.py
@@ -6,39 +6,39 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-#Enter your sending & receiving emails here
+# Enter your sending & receiving emails here
 sender = 'examplesender@gmail.com'
 password = 'YOUR_PASSWORD'
 receiver = ['examplerecipient@gmail.com', 'otherexamplerecipient@gmail.com']
 admin = 'exampleadmin@gmail.com'
 
-#Timestamp format from SQL export
-format = '%Y-%m-%d %H:%M:%S'
+# Timestamp format from SQL export
+formatDate = '%Y-%m-%d %H:%M:%S'
 now = datetime.now()
 
-#Name and path of .csv from SQL export
+# Name and path of .csv from SQL export
 fileName = '/var/tmp/timestamps_' + now.strftime('%Y_%m_%d') + '.csv'
 
-#Subject and body of email message
+# Subject and body of email message
 subject = 'Hours: ' + now.strftime('%B %-d, %Y')
 body = 'Here are the hours worked from the pay period ending ' + now.strftime('%B %-d, %Y') + ': \n'
 
 try:
-    #Main loop: summing hours for each employee and appending email body
+    # Main loop: summing hours for each employee and appending email body
     with open(fileName) as csvFile:
-        csvReader = csv.reader(csvFile, quoting = csv.QUOTE_ALL, delimiter = ',')
+        csvReader = csv.reader(csvFile, quoting=csv.QUOTE_ALL, delimiter=',')
         secondsWorked = 0
         row = csvReader.__next__()
         employee = row[1]
 
         while row[1] == employee:
-            timeStampBegin = datetime.strptime(row[2], format)
+            timeStampBegin = datetime.strptime(row[2], formatDate)
             name = row[3]
             row = csvReader.__next__()
-            timeStampEnd = datetime.strptime(row[2], format)
+            timeStampEnd = datetime.strptime(row[2], formatDate)
 
-            #Alert someone if unexpected timestamp difference
-            if ((((timeStampEnd - timeStampBegin).total_seconds())-1800) > 45000):
+            # Alert someone if unexpected timestamp difference
+            if (((timeStampEnd - timeStampBegin).total_seconds()) - 1800) > 45000:
                 subject = 'Error Summing Hours'
                 body = 'Excessive timestamp difference'
                 message = MIMEMultipart()
@@ -56,18 +56,18 @@ try:
                 except:
                     print('alert email failed')
 
-            secondsWorked += (((timeStampEnd - timeStampBegin).total_seconds())-1800)
+            secondsWorked += (((timeStampEnd - timeStampBegin).total_seconds()) - 1800)
             try:
                 row = csvReader.__next__()
                 if employee != row[1]:
-                    body += name + ': ' + str(timedelta(seconds = secondsWorked)) + '\n'
+                    body += name + ': ' + str(timedelta(seconds=secondsWorked)) + '\n'
                     employee = row[1]
                     secondsWorked = 0
             except:
-                body += name + ': ' + str(timedelta(seconds = secondsWorked)) + '\n'
+                body += name + ': ' + str(timedelta(seconds=secondsWorked)) + '\n'
                 break
 
-        #Constructing email
+        # Constructing email
         message = MIMEMultipart()
         message['From'] = sender
         message['To'] = ', '.join(receiver)
@@ -75,7 +75,7 @@ try:
         message.attach(MIMEText(body, 'plain'))
         bodyText = message.as_string()
 
-        #Connecting to SMTP and sending email
+        # Connecting to SMTP and sending email
         try:
             server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
             server.login(sender, password)
